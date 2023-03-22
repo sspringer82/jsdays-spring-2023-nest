af8b85ee3c354bf96e6833df86e0712cbc24d290
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-03-22 14:25:06 +0100

    api documentation

--- src/books/book.ts
+++ src/books/book.ts
@@ -1,17 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('books')
 export class Book {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
+
+  @ApiProperty({
+    description: 'Title of the book',
+    example: 'Die kleine Raupe Nimmersatt',
+  })
   @Column()
   title: string;
+
+  @ApiProperty()
   @Column()
   author: string;
+
+  @ApiProperty()
   @Column()
   price: number;
+
+  @ApiProperty()
   @Column()
   pages: number;
+
+  @ApiProperty({
+    description: 'Release year of the book',
+    example: 2022,
+  })
   @Column()
   year: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Library')
+    .setDescription('The fancy API of the world leading library')
+    .setVersion('0.0.1-beta.2')
+    .addTag('library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
